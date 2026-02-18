8488a9fe7531ff6a34b8c8ad9cc425c5687c3a64
Moni <monica.ibarra@iteso.mx>
2026-02-18 14:46:47 -0600

    Refactor code structure for improved readability and maintainability

--- image_generation/dataset.py
+++ image_generation/dataset.py
@@ -1,29 +1,54 @@
-from pathlib import Path
-
-from loguru import logger
-from tqdm import tqdm
-import typer
-
-from image_generation.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
-
-app = typer.Typer()
+from PIL import Image
+import os
+import pillow_avif
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+def preprocess_images(input_dir, output_dir, size=(64, 64)):
+    os.makedirs(output_dir, exist_ok=True)
 
+    extensiones = (".jpg", ".jpeg", ".png", ".avif")
+    total_found = 0
+    total_processed = 0
+
+    for file in os.listdir(input_dir):
+        if file.lower().endswith(extensiones):
+            total_found += 1
+            try:
+                path_in = os.path.join(input_dir, file)
+                path_out = os.path.join(
+                    output_dir,
+                    os.path.splitext(file)[0] + ".jpg"
+                )
+
+                img = Image.open(path_in).convert("RGB")
+
+                # Crop centrado para hacerla cuadrada
+                w, h = img.size
+                min_dim = min(w, h)
+
+                left = (w - min_dim) // 2
+                top = (h - min_dim) // 2
+                right = left + min_dim
+                bottom = top + min_dim
+
+                img = img.crop((left, top, right, bottom))
+
+                # Resize a 64x64
+                img = img.resize(size)
+
+                img.save(path_out, "JPEG")
+                total_processed += 1
+
+            except Exception as e:
+                print("Error con:", file, e)
+
+    print("Found images:", total_found)
+    print("Images processed correctly:", total_processed)
+    print("Preprocessing complete")
 
 if __name__ == "__main__":
-    app()
+    preprocess_images(
+        "image_generation/raw/original_tshirts",
+        "image_generation/processed/final_tshirts"
+    )
+
